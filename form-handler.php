6952803aeac3b1761e613e1d37892219c6c688f5
yanliu1111 <liuyan0504@gmail.com>
2023-03-13 16:05:14 -0600

    Fix: email

--- form-handler.php
+++ form-handler.php
@@ -4,7 +4,7 @@ $visitor_email = $_POST['email'];
 $subject = $_POST['subject'];
 $message = $_POST['message'];
 
-$email_from = 'infor@contactus.com';
+$email_from = 'liuyan0504@gmail.com';
 $email_subject = 'New Form Submission';
 $email_body = "User Name: $name.\n".
                 "User Email: $visitor_email.\n".
